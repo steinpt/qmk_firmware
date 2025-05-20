9eb95a5f310063e7d4858f6b1c985fabf18329e8
Stein Tronstad <tronstad@gmail.com>
2025-05-20 15:43:37 +0200

    added single quote

--- keyboards/lily58/keymaps/steinpt/keymap.c
+++ keyboards/lily58/keymaps/steinpt/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_BASE] = LAYOUT(
-  _______,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                             KC_6,    KC_7,         KC_8,         KC_9,         KC_0,            KC_MINS,
+  KC_NUBS,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                             KC_6,    KC_7,         KC_8,         KC_9,         KC_0,            KC_MINS,
   KC_TAB,   KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                             KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC,
   _______,  LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,                             KC_H,    RSFT_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
   _______,  KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,           _______, _______, KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_BSLS,
